bbda627bbcda02259bc3b1ce036139bc97dbc71c
Kaur <kun1333@hotmail.com>
2018-02-25 15:33:03 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import {Route} from 'react-router';
+import {HashRouter} from 'react-router-dom'
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import Immutable from 'immutable';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = combineReducers({
+
+});
+
+let initialState = { config: Immutable.Map(window.config) };
+
+console.log(window.config);
+console.log('context path: ' + contextPath);
+
+const middleware = [];
+const store = createStore(reducer, initialState, applyMiddleware(...middleware));
+
+ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <div>
+          <Route path='/' render={() => <View /> } />
+        </div>
+      </HashRouter>
+    </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
