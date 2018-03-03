2241c1913fe3a7dc158130f5d917807c18fb3e79
Kun1333 <kun1333@hotmail.com>
2018-03-03 14:38:36 +0200

    react views

---
+++ src/reducers/configReducer.js
@@ -0,0 +1,10 @@
+import Immutable from 'immutable'
+
+const INITIAL_STATE = Immutable.Map();
+
+export function configReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+
+  }
+  return state;
+}
---
+++ src/reducers/viewReducer.js
@@ -0,0 +1,10 @@
+import Immutable from 'immutable'
+
+const INITIAL_STATE = Immutable.Map();
+
+export function viewReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+
+  }
+  return state;
+}
