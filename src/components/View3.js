fa5f5b4d1c1e678a887a6b53ca8e0774722d69e0
Kun1333 <kun1333@hotmail.com>
2018-03-03 14:26:24 +0200

    react views

---
+++ src/components/View3.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Segment, Dimmer, Loader } from 'semantic-ui-react'
+
+import { View2 } from './View2'
+
+// import { getEvent } from './actions';
+
+
+class View3Internal extends React.Component {
+  componentWillMount() {
+    console.log('xx');
+  }
+
+  static get propTypes() {
+    return {
+      value: PropTypes.string.isRequired,
+      value2: PropTypes.string.isRequired,
+    }
+  }
+
+  render() {
+    return <div>
+      <View2 />
+      <div>{this.props.value}</div>
+      <div>{this.props.value2}</div>
+    </div>
+  }
+}
+
+export const View3 = connect(
+ state => {
+   return {
+     config: state.config,
+ };
+},
+ (dispatch, props) => {
+   return {
+     // getEvent: (url) => dispatch(getEvent(url))
+   };
+ }
+) (View3Internal);
